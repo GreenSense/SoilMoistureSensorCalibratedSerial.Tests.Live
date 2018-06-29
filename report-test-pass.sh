479c7885e9bb1c1558d7d10b9482fe9ef4448e1a
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-30 09:01:33 +1000

    Fixed test scripts

--- report-test-pass.sh
+++ report-test-pass.sh
@@ -9,5 +9,8 @@ MQTT_HOST=$(cat mqtt-host.security)
 MQTT_USERNAME=$(cat mqtt-username.security)
 MQTT_PASSWORD=$(cat mqtt-password.security)
 
+echo "Reporting test pass via MQTT"
+
 mosquitto_pub -h $MQTT_HOST -t "/$DEVICE_NAME/StatusMessage" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -m "Passed" && \
-mosquitto_pub -h $MQTT_HOST -t "/$DEVICE_NAME/Status" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -m "0"
+mosquitto_pub -h $MQTT_HOST -t "/$DEVICE_NAME/Status" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -m "0" && \
+mosquitto_pub -h $MQTT_HOST -t "/$DEVICE_NAME/Error" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -m ""
