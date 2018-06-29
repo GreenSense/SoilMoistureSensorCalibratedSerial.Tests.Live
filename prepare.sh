83a2b133224f8c70ff22330675c3d546970a794a
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-29 18:34:10 +1000

    Updated prepare script

--- prepare.sh
+++ prepare.sh
@@ -3,9 +3,33 @@ echo "Dir: $PWD"
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu stable-$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+if ! type "msbuild" > /dev/null; then
+  VERSION_NAME=$(lsb_release -cs)
 
-sudo apt-get update -qq && \
-sudo apt-get install -y --no-install-recommends git wget mono-complete msbuild ca-certificates-mono mosquitto-clients
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF && \
+  echo "deb https://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list && \
+  apt-get update -qq && \
+  apt-get install -y mono-complete ca-certificates-mono msbuild
+else
+  echo "Mono/msbuild is already installed. Skipping install."
+fi
+
+
+if ! type "mosquitto_pub" > /dev/null; then
+  sudo apt-get install -y --no-install-recommends mosquitto-clients
+else
+  echo "Mosquitto clients already installed. Skipping install."
+fi
+
+if ! type "git" > /dev/null; then
+  sudo apt-get install -y --no-install-recommends git
+else
+  echo "git is already installed. Skipping install."
+fi
+
+if ! type "wget" > /dev/null; then
+  sudo apt-get install -y --no-install-recommends wget
+else
+  echo "wget is already installed. Skipping install."
+fi
 
